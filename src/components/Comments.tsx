c2525ebc998d40f2a6af54c05fe16dab37411451
git <richard.james.grove@gmail.com>
2023-11-29 09:51:04 +0000

    update

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,10 +1,27 @@
+import { WEB_SITE } from "config";
 export default async function comments({ postSlug }: { postSlug: string }) {
+  const WEBSITE_URL = "http://localhost:3001";
+
+  let comments = [];
+
+  try {
+    const commentsResult = await fetch(
+      `${WEBSITE_URL}/api/comments/${postSlug}`,
+      { next: { revalidate: 5 } }
+    );
+    const response = await commentsResult.json();
+    console.log(response);
+    comments = response.comments.rows;
+  } catch (err) {
+    console.log(err);
+  }
+
   return (
     <div>
-      <h2>| commwnts |</h2>
+      <h2>| comments |</h2>
       <h3> Leave a comment</h3>
 
-      <form action={"/api/comments/${postSlug}"} method="POST">
+      <form action={`/api/comments/${postSlug}`} method="POST">
         <label htmlFor="username">Name</label>
         <input type="text" name="username" />
 
@@ -13,6 +30,16 @@ export default async function comments({ postSlug }: { postSlug: string }) {
 
         <button type="submit">send comment</button>
       </form>
+      {/* @ts-ignore */}
+      {comments.map((comment) => {
+        return (
+          <li key={comment.id}>
+            {comment.username} says...
+            <br />
+            {comment.content}
+          </li>
+        );
+      })}
     </div>
   );
 }
